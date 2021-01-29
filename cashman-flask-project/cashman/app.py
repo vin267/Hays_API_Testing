988a22f24e070e5c19552e26887dea7e289001e5
vin267 <vineet99583@gmail.com>
2021-01-29 10:44:38 -0800

    first commit

---
+++ cashman-flask-project/cashman/app.py
@@ -0,0 +1,73 @@
+from flask import Flask, jsonify, request
+
+from .model.expense import Expense, ExpenseSchema
+from .model.income import Income, IncomeSchema
+from .model.transaction_type import TransactionType
+
+import json
+import requests
+
+app = Flask(__name__)
+
+transactions = [
+    Income('Salary', 5000),
+    Income('Dividends', 200),
+    Expense('pizza', 50),
+    Expense('Rock Concert', 100)
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+    schema = IncomeSchema(many=True)
+    incomes = schema.dump(
+        filter(lambda t: t.type == TransactionType.INCOME, transactions)
+    )
+    return jsonify(incomes)
+
+
+@app.route('/api/ping')
+def add_income():
+    return "{success: true}", 200
+
+
+@app.route('/api/posts')
+def add_expenses():
+    tags = request.args.get('tags')
+    print(request.args)
+    sortBy = request.args.get('sortBy')
+    direction = request.args.get('direction')
+
+    response = requests.get("https://api.hatchways.io/assessment/blog/posts", params={'tag': tags})
+
+    json_data = json.loads(response.text)
+
+    for x, y in json_data.items():
+
+        print(x)
+
+    return str(response.text)
+
+
+"""
+@app.route('/expenses')
+def get_expenses():
+  schema = ExpenseSchema(many=True)
+  expenses = schema.dump(
+      filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
+  )
+  print("hello test", expenses)
+
+  return jsonify(expenses.data)
+"""
+
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense.data)
+    return "", 204
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ cashman-flask-project/playground.py
@@ -0,0 +1,7 @@
+import collections
+
+d = {2:3, 1:89, 4:5, 3:0}
+
+od = collections.Ordered
+print(od)
+#OrderedDict([(1, 89), (2, 3), (3, 0), (4, 5)])
